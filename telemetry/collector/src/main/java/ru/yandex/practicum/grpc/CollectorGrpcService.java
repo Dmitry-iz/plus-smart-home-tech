dc81986e75b83e9593fbaec9899cea432ce82ef8
Dmitry_iz <zzzufo@yandex.ru>
2025-11-03 15:56:51 +0300

    реализация на gRPC fix - 3

--- telemetry/collector/src/main/java/ru/yandex/practicum/grpc/CollectorGrpcService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/grpc/CollectorGrpcService.java
@@ -1,6 +1,5 @@
 //package ru.yandex.practicum.grpc;
 //
-//import io.grpc.Status;
 //import io.grpc.StatusRuntimeException;
 //import io.grpc.stub.StreamObserver;
 //import lombok.RequiredArgsConstructor;
@@ -9,19 +8,17 @@
 //import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
 //import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 //import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
-//import ru.yandex.practicum.model.HubEvent;
-//import ru.yandex.practicum.model.SensorEvent;
-//import ru.yandex.practicum.service.EventMapperService;
-//import ru.yandex.practicum.service.KafkaProducerService;
+//import ru.yandex.practicum.service.SensorEventHandler;
 //import com.google.protobuf.Empty;
+//
+//import java.util.Map;
 //
 //@Slf4j
 //@GrpcService
 //@RequiredArgsConstructor
 //public class CollectorGrpcService extends CollectorControllerGrpc.CollectorControllerImplBase {
 //
-//    private final KafkaProducerService kafkaProducerService;
-//    private final EventMapperService eventMapperService;
+//    private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorEventHandlers;
 //    private final GrpcToModelMapper grpcToModelMapper;
 //
 //    @Override
@@ -29,20 +26,18 @@
 //        try {
 //            log.info("Received sensor event via gRPC: {}", request.getId());
 //
-//            SensorEvent sensorEvent = grpcToModelMapper.toSensorEvent(request);
+//            // Используем обработчики как в задании
+//            if (sensorEventHandlers.containsKey(request.getPayloadCase())) {
+//                sensorEventHandlers.get(request.getPayloadCase()).handle(request);
+//            } else {
+//                throw new IllegalArgumentException("Не могу найти обработчик для события " + request.getPayloadCase());
+//            }
 //
-//            kafkaProducerService.sendSensorEvent(sensorEvent);
-//
-//            log.debug("Sensor event processed successfully via gRPC");
 //            responseObserver.onNext(Empty.getDefaultInstance());
 //            responseObserver.onCompleted();
 //        } catch (Exception e) {
 //            log.error("Failed to process sensor event via gRPC: {}", request, e);
-//            responseObserver.onError(new StatusRuntimeException(
-//                    Status.INTERNAL
-//                            .withDescription(e.getLocalizedMessage())
-//                            .withCause(e)
-//            ));
+//            responseObserver.onError(new StatusRuntimeException(io.grpc.Status.fromThrowable(e)));
 //        }
 //    }
 //
@@ -51,23 +46,19 @@
 //        try {
 //            log.info("Received hub event via gRPC: {}", request.getHubId());
 //
-//            HubEvent hubEvent = grpcToModelMapper.toHubEvent(request);
+//            // Для hub events пока используем старый подход
+//            var hubEvent = grpcToModelMapper.toHubEvent(request);
+//            // Здесь нужно будет добавить HubEventHandlers аналогично SensorEventHandlers
 //
-//            kafkaProducerService.sendHubEvent(hubEvent);
-//
-//            log.debug("Hub event processed successfully via gRPC");
 //            responseObserver.onNext(Empty.getDefaultInstance());
 //            responseObserver.onCompleted();
 //        } catch (Exception e) {
 //            log.error("Failed to process hub event via gRPC: {}", request, e);
-//            responseObserver.onError(new StatusRuntimeException(
-//                    Status.INTERNAL
-//                            .withDescription(e.getLocalizedMessage())
-//                            .withCause(e)
-//            ));
+//            responseObserver.onError(new StatusRuntimeException(io.grpc.Status.fromThrowable(e)));
 //        }
 //    }
 //}
+
 
 package ru.yandex.practicum.grpc;
 
@@ -92,21 +83,11 @@ public class CollectorGrpcService extends CollectorControllerGrpc.CollectorContr
     private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorEventHandlers;
     private final GrpcToModelMapper grpcToModelMapper;
 
-//    public CollectorGrpcService(Set<SensorEventHandler> sensorEventHandlers, GrpcToModelMapper grpcToModelMapper) {
-//        this.grpcToModelMapper = grpcToModelMapper;
-//        this.sensorEventHandlers = sensorEventHandlers.stream()
-//                .collect(Collectors.toMap(
-//                        SensorEventHandler::getMessageType,
-//                        handler -> handler
-//                ));
-//    }
-
     @Override
-    public void sendSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
+    public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
         try {
             log.info("Received sensor event via gRPC: {}", request.getId());
 
-            // Используем обработчики как в задании
             if (sensorEventHandlers.containsKey(request.getPayloadCase())) {
                 sensorEventHandlers.get(request.getPayloadCase()).handle(request);
             } else {
@@ -122,7 +103,7 @@ public class CollectorGrpcService extends CollectorControllerGrpc.CollectorContr
     }
 
     @Override
-    public void sendHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
+    public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
         try {
             log.info("Received hub event via gRPC: {}", request.getHubId());
 
