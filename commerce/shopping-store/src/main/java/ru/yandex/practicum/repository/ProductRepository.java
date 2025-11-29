ab35c0c48350357bfd538c850ad393a24407c331
Dmitry_iz <zzzufo@yandex.ru>
2025-11-29 14:10:40 +0300

    реализация тз 21

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/repository/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/repository/ProductRepository.java
@@ -6,33 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import ru.yandex.practicum.entity.Product;
-import ru.yandex.practicum.dto.shoppingstore.ProductCategory;
-import ru.yandex.practicum.dto.shoppingstore.ProductState;
 
 import java.util.List;
 import java.util.UUID;
 
-//public interface ProductRepository extends JpaRepository<Product, UUID> {
-//
-//    Page<Product> findByProductCategoryAndProductState(
-//            ProductCategory productCategory,
-//            ProductState productState,
-//            Pageable pageable
-//    );
-//
-//    List<Product> findByProductState(ProductState productState);
-//
-//    @Query("SELECT p FROM Product p WHERE p.productCategory = :category AND p.productState = 'ACTIVE'")
-//    List<Product> findActiveProductsByCategory(@Param("category") ProductCategory category);
-//
-//    boolean existsByProductIdAndProductState(UUID productId, ProductState productState);
-//}
-
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     Page<Product> findByProductCategoryAndProductState(
-            ru.yandex.practicum.dto.shoppingstore.ProductCategory productCategory,  // DTO enum
-            ru.yandex.practicum.dto.shoppingstore.ProductState productState,        // DTO enum
+            ru.yandex.practicum.dto.shoppingstore.ProductCategory productCategory,
+            ru.yandex.practicum.dto.shoppingstore.ProductState productState,
             Pageable pageable
     );
 
